42aaa326961b022df40eb0f2fe9fe848f31cd605
kjinn62 <kjinn62@gmail.com>
2019-11-19 13:56:07 -0800

    little progress on ajax implementation

--- apps/webframe/urls.py
+++ apps/webframe/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^$', views.homepage),
     url(r'^images$', views.image),
     url(r'^live$', views.webcam),
-    url(r'^upload$', views.upload, name = 'upload'),
+    url(r'^upload$', views.upload),
 ]
